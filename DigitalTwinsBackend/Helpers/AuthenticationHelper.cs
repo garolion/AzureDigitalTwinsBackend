e6eb86ac2627f2b0731298d410a7a0d3e141f126
Arnaud Fontaine <arfontai@microsoft.com>
2018-12-11 18:15:24 +0100

    Cleaning

--- DigitalTwinsBackend/Helpers/AuthenticationHelper.cs
+++ DigitalTwinsBackend/Helpers/AuthenticationHelper.cs
@@ -8,54 +8,25 @@ namespace DigitalTwinsBackend.Helpers
 {
     public class AuthenticationHelper
     {
-        //TODO supprimer les contantes ci-dessous
-        //static readonly string AADInstance = "https://login.microsoftonline.com/";
-        //static readonly string ClientId = "f7400bff-2504-4e5f-8587-eda134e5a70d";
-        //static readonly string Resource = "0b07f429-9f4b-4714-9392-cc5e8e80c8b0";
-        //static readonly string Tenant = "72f988bf-86f1-41af-91ab-2d7cd011db47";
-        //static readonly string BaseUrl = "https://sh360iot-digitaltwins-o3e7wjxah3yqg.northeurope.azuresmartspaces.net/management/api/v1.0/";
-
-        //static string Authority => AADInstance + Tenant;
-
-        //public async Task<HttpClient> SetupHttpClient(ILogger logger)
-        //{
-        //    var httpClient = new HttpClient()
-        //    {
-        //        BaseAddress = new Uri(BaseUrl),
-        //    };
-        //    var accessToken = (await AuthenticationHelper.GetToken(logger, BaseUrl, Authority, Resource, ClientId));
-
-        //    httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-        //    return httpClient;
-        //}
-
-        //private HttpClient httpClient;
-
         public static async Task<HttpClient> SetupHttpClient(ILogger logger)
         {
-            //if (httpClient != null)
-            //{
-            //    return httpClient;
-            //}
-            //else
-            //{
             HttpClient httpClient;
 
-                string authority = ConfigHelper.Config.parameters.AADInstance + ConfigHelper.Config.parameters.Tenant;
+            string authority = ConfigHelper.Config.parameters.AADInstance + ConfigHelper.Config.parameters.Tenant;
 
-                httpClient = new HttpClient()
-                {
-                    BaseAddress = new Uri(ConfigHelper.Config.parameters.BaseUrl),
-                };
-                var accessToken = (await AuthenticationHelper.GetToken(
-                    logger,
-                    ConfigHelper.Config.parameters.BaseUrl,
-                    authority,
-                    ConfigHelper.Config.parameters.Resource,
-                    ConfigHelper.Config.parameters.ClientId));
+            httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(ConfigHelper.Config.parameters.BaseUrl),
+            };
+            var accessToken = (await AuthenticationHelper.GetToken(
+                logger,
+                ConfigHelper.Config.parameters.BaseUrl,
+                authority,
+                ConfigHelper.Config.parameters.Resource,
+                ConfigHelper.Config.parameters.ClientId));
 
-                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-                return httpClient;
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+            return httpClient;
         }
 
         // Gets an access token
